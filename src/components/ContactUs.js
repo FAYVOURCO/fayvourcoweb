cc39f223f25fa5b795efd0b70bef304c9ba3cda0
phayvie <fayvourco@gmail.com>
2023-08-06 14:47:41 +0100

    file structure

---
+++ src/components/ContactUs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./ContactUs.css";
+ 
+
+function ContactUs() {
+  return (
+    <div>
+      CONTACT US PAGE
+    </div>
+
+  )
+}
+
+export default ContactUs;
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Home.css";
+ 
+
+function Home() {
+  return (
+    <div>
+      WE ARE THE GARAGE WHERE IDEAS ARE BORN,
+      <br/>
+      THE C-SUITE WHERE THEY ARE EXECUTED,
+      <br/>
+      AND THE DREAM TEAM THAT MAKES THEM A REALITY.
+    </div>
+
+  )
+}
+
+export default Home;
